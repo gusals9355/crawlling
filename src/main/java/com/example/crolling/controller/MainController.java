6bb8ea06e529a22061a39ea75d5c7c00cd72e047
gusals9355 <gusals9355@naver.com>
2021-07-23 12:33:21 +0900

    유튜브 크롤링 완2

--- src/main/java/com/example/crolling/controller/MainController.java
+++ src/main/java/com/example/crolling/controller/MainController.java
@@ -26,22 +26,25 @@ public class MainController {
     public String index(String text, Model model) throws IOException {
         List<String> hrefList = new ArrayList();
         List<String> ImgList = new ArrayList();
+        List<String> titleList = new ArrayList();
+        List<String> writerList = new ArrayList();
 //        Map<String, Object> map = new HashMap<>();
         String url = "https://www.google.com/search?q="+text+"+%EC%9C%A0%ED%8A%9C%EB%B8%8C&sxsrf=ALeKk01eqJ-QQtPbqLXUjxiIRUT3UneyOQ:1626873181915&source=lnms&tbm=vid&sa=X&ved=2ahUKEwi81ejwnvTxAhURxYsBHTYBBWgQ_AUoAXoECAEQAw&biw=1286&bih=788";
         Document doc = Jsoup.connect(url).get();
         Elements el = doc.select("a.rGhul");
+        String[] cnt = ((doc.select("div.ij69rd.UHe5G")).text().split(" ")); //조회수
         for (Element e:el) {
             hrefList.add(e.attr("href"));
             Document doc2 = Jsoup.connect(e.attr("href")).get();
-            Elements el2 = doc2.select("link[itemprop=thumbnailUrl]");
-            for(Element e2:el2){
-                ImgList.add(e2.attr("href"));
-            }
+            ImgList.add(doc2.select("link[itemprop=thumbnailUrl]").attr("href")); //썸네일
+            titleList.add(doc2.title().substring(0,doc2.title().length()-10)); //제목
+            writerList.add(doc2.select("link[itemprop=name]").attr("content")); //작성자
         }
-//        map.put("hrefList",hrefList);
-//        map.put("ImgList",ImgList);
         model.addAttribute("hrefList",hrefList);
         model.addAttribute("ImgList",ImgList);
+        model.addAttribute("titleList",titleList);
+        model.addAttribute("writerList",writerList);
+        model.addAttribute("cntList",cnt);
         return "index";
     }
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -13,7 +13,11 @@
     </div>
     <div>
         <c:forEach var="i" items="${hrefList}" varStatus="status">
-            <a href="<c:out value="${i}"></c:out>"><img src="${ImgList[status.index]}"></a><br>
+            <a href="<c:out value="${i}"></c:out>"><img src="${ImgList[status.index]}" width="500px" height="300px"><br>
+                <c:out value="${writerList[status.index]}"></c:out>&nbsp;&nbsp;<c:out value="${cntList[status.index]}"></c:out><br>
+                <c:out value="${titleList[status.index]}"></c:out>
+            </a><br>
+
         </c:forEach>
     </div>
 </body>
